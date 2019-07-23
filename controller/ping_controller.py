611a26c7cb543ab6b2d879aeb4403e69aa1f9701
hama-matcha <hamamatcha@gmail.com>
2019-07-23 12:21:35 +0900

    :sparkles:ping_controllerの具体化クラスを実装

--- controller/ping_controller.py
+++ controller/ping_controller.py
@@ -3,6 +3,10 @@ from injector import Injector, inject
 from flask import jsonify
 
 class PingControllerInterface(metaclass=ABCMeta):
+    """
+    @param: None\n
+    @return: Json
+    """
     @abstractmethod
     def pong(self):
         pass
@@ -12,7 +16,12 @@ class PingController():
     def __init__(self, i: PingControllerInterface):
         if not isinstance(i, PingControllerInterface):
             raise Exception("i is not Interface of Ping")
-    
+        self.pci = i
+
+    def pong(self):
+        return self.pci.pong()
+
+class PingPong(PingControllerInterface):
     def pong(self):
         result = jsonify({'message':"pong"})
         return result
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import jsonify, Flask
+from controller.ping_controller import PingPong
+
+ping_c = PingPong()
 
 app = Flask(__name__)
 
+@app.route('/ping', methods=['GET'])
+def pin_pon():
+    return ping_c.pong()
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
